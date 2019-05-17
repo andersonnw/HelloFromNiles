8ab539d6816f29a7ca8e0eff7f2013705777a910
kovalyovi <ilia.kovaliov@gmail.com>
2019-05-17 09:19:42 -0600

    A little update

--- src/HelloFromNiles/Main.java
+++ src/HelloFromNiles/Main.java
@@ -3,6 +3,7 @@ package HelloFromNiles;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello from Niles.");
+	    System.out.println("Hello from Niles.");
+        System.out.println("Hello Niles, my name is Ilya");
     }
 }
